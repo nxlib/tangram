b0ee59f53aaa6120fc7d861497e6c12b628c8ae6
Garming <garming@msn.com>
2017-05-04 10:30:41 +0800

    `create`

--- src/Tangram/Command/Create/Create.php
+++ src/Tangram/Command/Create/Create.php
@@ -10,43 +10,73 @@ namespace Tangram\Command\Create;
 
 
 use Tangram\Utils\Dir;
+use Tangram\Utils\File;
 
 class Create
 {
-    public static function run($modulePath,$restfulPath,$webPagePath){
-        console($modulePath);
-        console($restfulPath);
-        console($webPagePath);
+    private static $moduleName;
+    private static $module;
+    private static $upModule;
+    private static $lowModule;
 
-        $module = explode("/",TG_MODULE);
-        $module = end($module);
-        $module = lcfirst($module);
+    public static function run($modulePath, $restfulPath, $webPagePath)
+    {
+        static::$moduleName = TG_MODULE;
+        $module = explode("/", TG_MODULE);
+        static::$module = end($module);
+        static::$upModule = ucfirst(static::$module);
+        static::$lowModule = lcfirst(static::$module);
 
-        $modulePath = TG_RUN_PATH.DIRECTORY_SEPARATOR.$modulePath.DIRECTORY_SEPARATOR.$module;
-        $restfulPath = TG_RUN_PATH.DIRECTORY_SEPARATOR.$restfulPath.DIRECTORY_SEPARATOR.$module;
-        $webPagePath = TG_RUN_PATH.DIRECTORY_SEPARATOR.$webPagePath.DIRECTORY_SEPARATOR.$module;
+        $modulePath = TG_RUN_PATH . DIRECTORY_SEPARATOR . $modulePath . DIRECTORY_SEPARATOR . static::$module;
+        $restfulPath = TG_RUN_PATH . DIRECTORY_SEPARATOR . $restfulPath . DIRECTORY_SEPARATOR . static::$module;
+        $webPagePath = TG_RUN_PATH . DIRECTORY_SEPARATOR . $webPagePath . DIRECTORY_SEPARATOR . static::$module;
 
         $all_dir = [
             $modulePath,
-            $modulePath.DIRECTORY_SEPARATOR."entity",
-            $modulePath.DIRECTORY_SEPARATOR."model",
-            $modulePath.DIRECTORY_SEPARATOR."service",
-            $modulePath.DIRECTORY_SEPARATOR."exception",
+            $modulePath . DIRECTORY_SEPARATOR . "entity",
+            $modulePath . DIRECTORY_SEPARATOR . "model",
+            $modulePath . DIRECTORY_SEPARATOR . "service",
+            $modulePath . DIRECTORY_SEPARATOR . "exception",
             $restfulPath,
-            $restfulPath.DIRECTORY_SEPARATOR."controller",
+            $restfulPath . DIRECTORY_SEPARATOR . "controller",
             $webPagePath,
-            $webPagePath.DIRECTORY_SEPARATOR."controller",
-            $webPagePath.DIRECTORY_SEPARATOR."views",
+            $webPagePath . DIRECTORY_SEPARATOR . "controller",
+            $webPagePath . DIRECTORY_SEPARATOR . "views",
         ];
-        foreach ($all_dir as $v){
+        foreach ($all_dir as $v) {
             Dir::create($v);
         }
+        if(!file_exists($modulePath . DIRECTORY_SEPARATOR . "tangram.json")){
+            File::create($modulePath . DIRECTORY_SEPARATOR . "tangram.json", self::moduleTangramJsonFile());
+            File::create($modulePath . DIRECTORY_SEPARATOR . "entity" . DIRECTORY_SEPARATOR . static::$upModule . ".php", self::entityFile());
+            File::create($modulePath . DIRECTORY_SEPARATOR . "model" . DIRECTORY_SEPARATOR . static::$upModule . "Model.php", self::modelFile());
+            File::create($modulePath . DIRECTORY_SEPARATOR . "service" . DIRECTORY_SEPARATOR . static::$upModule . "Service.php", self::serviceFile());
+        }else{
+            console("module is exist");
+        }
+        if(!file_exists($restfulPath . DIRECTORY_SEPARATOR . "tangram.json")){
+            File::create($restfulPath . DIRECTORY_SEPARATOR . "tangram.json", self::restfulTangramJsonFile());
+            File::create($restfulPath . DIRECTORY_SEPARATOR . "controller" . DIRECTORY_SEPARATOR . static::$upModule . "Controller.php", self::restFile());
+        }else{
+            console("restful is exist");
+        }
+        if(!file_exists($webPagePath . DIRECTORY_SEPARATOR . "tangram.json")){
+            File::create($webPagePath . DIRECTORY_SEPARATOR . "tangram.json", self::webPageTangramJsonFile());
+            File::create($webPagePath . DIRECTORY_SEPARATOR . "controller" . DIRECTORY_SEPARATOR . static::$upModule . "Controller.php", self::controllerFile());
+        }else{
+            console("web-page is exist");
+        }
+
+
+
 
     }
-    private static function webPageTangramJsonFile($moduleName){
-        $module = explode("/",$moduleName);
-        $module = end($module);
-        $upName = ucfirst($module);
+
+    private static function webPageTangramJsonFile()
+    {
+        $moduleName = static::$moduleName;
+        $module = static::$module;
+        $upName = static::$upModule;
         return <<<"EOF"
 {
   "name": "{$moduleName}",
@@ -69,16 +99,18 @@ class Create
   "minimum-stability": "dev",
   "autoload": {
     "psr-4": {
-      "{$upName}\\Controller\\": "controller"
+      "{$upName}\\\\Controller\\\\": "controller"
     }
   }
 }
 EOF;
     }
-    private static function restfulTangramJsonFile($moduleName){
-        $module = explode("/",$moduleName);
-        $module = end($module);
-        $upName = ucfirst($module);
+
+    private static function restfulTangramJsonFile()
+    {
+        $moduleName = static::$moduleName;
+        $module = static::$module;
+        $upName = static::$upModule;
         return <<<"EOF"
 {
   "name": "{$moduleName}",
@@ -101,16 +133,18 @@ EOF;
   "minimum-stability": "dev",
   "autoload": {
     "psr-4": {
-      "{$upName}\\RestController\\": "controller"
+      "{$upName}\\\\RestController\\\\": "controller"
     }
   }
 }
 EOF;
     }
-    private static function moduleTangramJsonFile($moduleName){
-        $module = explode("/",$moduleName);
-        $module = end($module);
-        $upName = ucfirst($module);
+
+    private static function moduleTangramJsonFile()
+    {
+        $moduleName = static::$moduleName;
+        $module = static::$module;
+        $upName = static::$upModule;
         return <<<"EOF"
 {
   "name": "{$moduleName}",
@@ -133,9 +167,114 @@ EOF;
   "minimum-stability": "dev",
   "autoload": {
     "psr-4": {
-      "{$upName}\\": ""
+      "{$upName}\\\\": ""
     }
   }
+}
+EOF;
+    }
+
+    private static function entityFile()
+    {
+        $upName = static::$upModule;
+        $date = date("Y-m-d H:i:s", time());
+        return <<<"EOF"
+<?php
+/**
+ * User: Tangram Auto Creator
+ * Date: {$date}
+ */
+
+namespace {$upName}\Entity;
+
+
+class {$upName}
+{
+    
+}
+EOF;
+    }
+
+    private static function modelFile()
+    {
+        $upName = static::$upModule;
+        $date = date("Y-m-d H:i:s", time());
+        return <<<"EOF"
+<?php
+/**
+ * User: Tangram Auto Creator
+ * Date: {$date}
+ */
+
+namespace {$upName}\Model;
+
+
+class {$upName}Model
+{
+    
+}
+EOF;
+    }
+
+    private static function serviceFile()
+    {
+        $upName = static::$upModule;
+        $date = date("Y-m-d H:i:s", time());
+        return <<<"EOF"
+<?php
+/**
+ * User: Tangram Auto Creator
+ * Date: {$date}
+ */
+
+namespace {$upName}\Service;
+
+
+class {$upName}Service
+{
+    
+}
+EOF;
+    }
+
+    private static function restFile()
+    {
+        $upName = static::$upModule;
+        $date = date("Y-m-d H:i:s", time());
+        return <<<"EOF"
+<?php
+/**
+ * User: Tangram Auto Creator
+ * Date: {$date}
+ */
+
+namespace {$upName}\RestController;
+
+
+class {$upName}Controller
+{
+    
+}
+EOF;
+    }
+
+    private static function controllerFile()
+    {
+        $upName = static::$upModule;
+        $date = date("Y-m-d H:i:s", time());
+        return <<<"EOF"
+<?php
+/**
+ * User: Tangram Auto Creator
+ * Date: {$date}
+ */
+
+namespace {$upName}\Controller;
+
+
+class {$upName}Controller
+{
+    
 }
 EOF;
     }
