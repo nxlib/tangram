6547a486b3cdb726dac88bd56949a8937715e425
Garming <garming@msn.com>
2018-02-28 14:23:18 +0800

    coding

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,10 +9,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-function console($data){
-    print_r($data);
-    echo "\r\n";
-}
 function includeIfExists($file)
 {
   return file_exists($file) ? include $file : false;
